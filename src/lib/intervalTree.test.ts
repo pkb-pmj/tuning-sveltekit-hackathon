d134231782b7f09509d6bf97d090721c1c81c326
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2023-03-30 18:59:03 +0200

    ♻️ make interval tree a store - per-instance sorted lists + per-tree sorted list

--- src/lib/intervalTree.test.ts
+++ src/lib/intervalTree.test.ts
@@ -1,12 +1,38 @@
+import { get } from 'svelte/store';
 import { expect, test } from 'vitest';
 import { Interval } from './interval';
-import { Node } from './intervalTree';
+import { intervalTree, type Node } from './intervalTree';
 
-test('flat', () => {
-	const root = new Node(new Interval(1), null);
+function intervalsFromStrings(fractions: string[]): Interval[] {
+	return fractions.map((frac) => new Interval(frac));
+}
+
+function intervalsFromNodes(nodes: Node[]): Interval[] {
+	return nodes.map((node) => node.absInterval());
+}
+
+test('only root', () => {
+	const tree = intervalTree();
+	const list = get(tree);
+	expect(list.length).toStrictEqual(1);
+	expect(list[0].absInterval()).toEqual(new Interval(1));
+});
+
+test('flat tree', () => {
+	const tree = intervalTree();
+	let list = get(tree);
+	const [root] = list;
+	tree.subscribe((value) => (list = value));
+
 	root.addChild(new Interval('3/2'));
+	expect(intervalsFromNodes(list)).toEqual(intervalsFromStrings(['1', '3/2']));
+
 	root.addChild(new Interval('4/3'));
+	expect(intervalsFromNodes(list)).toEqual(intervalsFromStrings(['1', '4/3', '3/2']));
+
 	root.addChild(new Interval('2/3'));
-	const expected = ['2/3', '1', '4/3', '3/2'].map((frac) => new Interval(frac));
-	expect(root.sorted.map((node) => node.absInterval())).toEqual(expected);
+	expect(intervalsFromNodes(list)).toEqual(intervalsFromStrings(['2/3', '1', '4/3', '3/2']));
+
+	root.addChild(new Interval('3/4'));
+	expect(intervalsFromNodes(list)).toEqual(intervalsFromStrings(['2/3', '3/4', '1', '4/3', '3/2']));
 });
--- src/lib/intervalTree.ts
+++ src/lib/intervalTree.ts
@@ -1,57 +1,76 @@
-import type { Interval } from './interval';
+import { writable, type Readable } from 'svelte/store';
+import { Interval } from './interval';
 
-export class Node {
-	private interval: Interval;
-	private parent: Node | null;
-	private children: Node[] = [];
-	sorted: Node[] = [this];
+export interface Node {
+	addChild(interval: Interval): void;
+	removeSelf(): void;
+	updateInterval(interval: Interval): void;
+	absInterval(): Interval;
+}
 
-	constructor(interval: Interval, parent: Node | null) {
-		this.interval = interval;
-		this.parent = parent;
-	}
+export function intervalTree(): Readable<Node[]> {
+	const sorted: Node[] = [];
 
-	addChild(interval: Interval) {
-		const child = new Node(interval, this);
-		this.children.push(child);
-		this.sorted.push(child);
-		this.sorted.sort((a, b) => a.absInterval().log2valueOf() - b.absInterval().log2valueOf());
-	}
+	class Node {
+		private interval: Interval;
+		private parent: Node | null;
+		private children: Node[] = [];
 
-	removeSelf() {
-		let index = this.parent?.children.indexOf(this);
-		if (index === undefined) throw new Error("can't remove the root node");
-		if (index === -1) throw new Error("didn't find self in parent's children");
-		this.parent!.children.splice(index);
-
-		// TODO: reduce the complexity
-		const notes = [];
-		const queue: Node[] = [this];
-		while (queue.length > 0) {
-			const node = queue.shift()!;
-			notes.push(node);
-			node.children.forEach((child) => queue.push(child));
+		constructor(interval: Interval, parent: Node | null) {
+			this.interval = interval;
+			this.parent = parent;
 		}
-		notes.forEach((note) => {
-			const index = this.sorted.indexOf(note);
-			if (index === -1) throw new Error("didn't find self in sorted");
-			this.sorted.splice(index);
-		});
-		// no need to sort because we only removed some nodes, so the remaining nodes are still sorted
-	}
 
-	updateInterval(interval: Interval) {
-		this.interval = interval;
-		this.sorted.sort((a, b) => a.absInterval().log2valueOf() - b.absInterval().log2valueOf());
-	}
-
-	absInterval() {
-		let interval = this.interval;
-		let node = this.parent;
-		while (node) {
-			interval = interval.add(node.interval);
-			node = node.parent;
+		addChild(interval: Interval) {
+			const child = new Node(interval, this);
+			this.children.push(child);
+			sorted.push(child);
+			sorted.sort((a, b) => a.absInterval().log2valueOf() - b.absInterval().log2valueOf());
+			set(sorted);
 		}
-		return interval;
+
+		removeSelf() {
+			let index = this.parent?.children.indexOf(this);
+			if (index === undefined) throw new Error("can't remove the root node");
+			if (index === -1) throw new Error("didn't find self in parent's children");
+			this.parent!.children.splice(index);
+
+			// TODO: reduce the complexity
+			const notes = [];
+			const queue: Node[] = [this];
+			while (queue.length > 0) {
+				const node = queue.shift()!;
+				notes.push(node);
+				node.children.forEach((child) => queue.push(child));
+			}
+			notes.forEach((note) => {
+				const index = sorted.indexOf(note);
+				if (index === -1) throw new Error("didn't find self in sorted");
+				sorted.splice(index);
+			});
+			// no need to sort because we only removed some nodes, so the remaining nodes are still sorted
+			set(sorted);
+		}
+
+		updateInterval(interval: Interval) {
+			this.interval = interval;
+			sorted.sort((a, b) => a.absInterval().log2valueOf() - b.absInterval().log2valueOf());
+			set(sorted);
+		}
+
+		absInterval() {
+			let interval = this.interval;
+			let node = this.parent;
+			while (node) {
+				interval = interval.add(node.interval);
+				node = node.parent;
+			}
+			return interval;
+		}
 	}
+
+	sorted.push(new Node(new Interval(1), null));
+	const { set, subscribe } = writable(sorted);
+
+	return { subscribe };
 }
