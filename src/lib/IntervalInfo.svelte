d77f4f3369049b40b6cdc51b9b3488c7fc5c98c9
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2023-04-12 22:28:07 +0200

    💄 🐛 improve IntervalInfo formatting

--- src/lib/IntervalInfo.svelte
+++ src/lib/IntervalInfo.svelte
@@ -8,16 +8,25 @@
 	$: ({ pure, remainder, comma } = splitIntoPureAndComma(interval));
 </script>
 
-<IntervalMathMl {interval} display="inline" vertical />
-{interval.cents().toFixed(2)} cents
-{pure.name}
-{#if comma === null}
-	{remainder.log2valueOf() < 0 ? '-' : '+'}
-	<IntervalMathMl interval={remainder} display="inline" vertical />
-{:else if comma.diff.n !== 0}
-	{comma.diff.s === -1 ? '-' : '+'}
-	{#if comma.diff.n !== comma.diff.d}
-		{comma.diff.n}/{comma.diff.d}
-	{/if}
-	{comma.name}
-{/if}
+<div class="wrapper">
+	<div class="row">
+		<IntervalMathMl {interval} display="inline" vertical />
+	</div>
+	<div class="row">
+		{interval.cents().toFixed(2)} cents
+	</div>
+	<div class="row">
+		{#if pure.value.log2valueOf() !== 0 || comma?.diff.n === 0}
+			{pure.name}
+			{remainder.log2valueOf() < 0 ? '-' : remainder.log2valueOf() > 0 ? '+' : ''}
+		{/if}
+		{#if comma === null}
+			<IntervalMathMl interval={remainder} display="inline" vertical />
+		{:else if comma.diff.n !== 0}
+			{#if comma.diff.n !== comma.diff.d}
+				{comma.diff.n}/{comma.diff.d}
+			{/if}
+			{comma.name}
+		{/if}
+	</div>
+</div>
