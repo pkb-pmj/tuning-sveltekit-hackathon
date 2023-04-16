fc7eeabe0036cbcf01db546208e3b5517160c4fa
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2023-04-16 15:52:22 +0200

    💄 ♻️ extract toolbar to separate component

--- src/lib/ChromaticCircle.svelte
+++ src/lib/ChromaticCircle.svelte
@@ -8,37 +8,19 @@
 	import { keyLabelsEn, type KeyboardStore } from './keyboard';
 	import Waveform from './Waveform.svelte';
 	import IntervalArc from './IntervalArc.svelte';
-	import IntervalInfo from './IntervalInfo.svelte';
 	import { setContext } from 'svelte';
-	import { pureIntervals } from './pureIntervals';
+	import Toolbar from './Toolbar.svelte';
 
 	export let tree: Readable<Node[]>;
 	export let keyboard: KeyboardStore;
 
 	const selectedInterval = writable<Interval | null>(null);
-	setContext('selectedInterval', selectedInterval);
 	const storedInterval = writable<Interval | null>(null);
-	const selected = writable<Node[]>([]);
-
-	function storeInterval() {
-		$storedInterval = $selectedInterval;
-	}
-	function clearStoredInterval() {
-		$storedInterval = null;
-	}
-	function multiplyStoredInterval() {
-		$storedInterval = $storedInterval!.mul(factor);
-	}
-	function divideStoredInterval() {
-		$storedInterval = $storedInterval!.div(factor);
-	}
-	$: {
-		if ($selected.length === 2) {
-			$selectedInterval = $selected[0].absInterval.sub($selected[1].absInterval).modSigned().abs();
-		} else if ($selected.length > 2) {
-			$selectedInterval = null;
-		}
-	}
+	const selectedNotes = writable<Node[]>([]);
+	setContext('intervalTree', tree);
+	setContext('selectedInterval', selectedInterval);
+	setContext('storedInterval', storedInterval);
+	setContext('selectedNotes', selectedNotes);
 
 	const cmp = (a: Node, b: Node) =>
 		a.absInterval.modUnsigned().log2valueOf() - b.absInterval.modUnsigned().log2valueOf();
@@ -52,7 +34,7 @@
 			.sort(cmp),
 	);
 
-	const active = derived([playing, selected, tree], ([playing, selected, tree]) => {
+	const active = derived([playing, selectedNotes, tree], ([playing, selected, tree]) => {
 		return tree.filter((node) => playing.includes(node) || selected.includes(node)).sort(cmp);
 	});
 
@@ -73,112 +55,20 @@
 		return playing.map((node) => node.absInterval.modUnsigned().valueOf() * 256);
 	});
 
-	let intervalIndex = 7;
-	$: interval = pureIntervals[intervalIndex + 1].value;
-	let factor = 2;
-
-	function selectAll() {
-		$selected = $tree.slice();
-	}
-	function addChildUp() {
-		$selected.forEach((child) => child.addChild(interval));
-	}
-	function addChildDown() {
-		$selected.forEach((child) => child.addChild(interval.mul(-1)));
-	}
-	function updateInterval() {
-		$selected.forEach((child) =>
-			child.setInterval(interval.mul(Math.sign(child.relInterval.log2valueOf()))),
-		);
-	}
-	function removeSelf() {
-		$selected.forEach((child) => child.removeSelf());
-	}
 	function unselect(event: MouseEvent) {
 		if (event.shiftKey) return;
-		$selected = [];
+		$selectedNotes = [];
 		$selectedInterval = null;
-	}
-	function addToSelected() {
-		$selected.forEach((node) => {
-			const base = node.relInterval;
-			const sign = Math.sign(base.log2valueOf());
-			node.setInterval(base.add($storedInterval!.mul(sign)));
-		});
-	}
-	function subtractFromSelected() {
-		$selected.forEach((node) => {
-			const base = node.relInterval;
-			const sign = Math.sign(base.log2valueOf());
-			node.setInterval(base.sub($storedInterval!.mul(sign)));
-		});
 	}
 
 	const cents = Array(240)
 		.fill(0)
 		.map((_, i) => i * 5);
-
-	function onKeyDown(event: KeyboardEvent) {
-		if (event.code === 'Backspace') removeSelf();
-	}
 </script>
 
-<svelte:window on:keydown={onKeyDown} />
 <Waveform {frequencies} />
 <div class="wrapper">
-	<div class="toolbar">
-		<div class="intervalBox">
-			{#if $selected.length > 0}
-				<select bind:value={intervalIndex}>
-					{#each pureIntervals.slice(1, -1) as pure, i}
-						<option value={i}>
-							{pure.value.frac()} – {pure.name}
-						</option>
-					{/each}
-				</select>
-				<div class="row">
-					<button on:click={addChildUp}>Add note up</button>
-					<button on:click={addChildDown}>Add note down</button>
-				</div>
-				<button on:click={updateInterval} style:background-color="#48f8">Update interval</button>
-				<button on:click={removeSelf} style:background-color="#f008">Remove</button>
-			{:else}
-				<button on:click={selectAll} style:background-color="#48f8">Select all notes</button>
-				<span>Select and edit a note or multiple notes</span>
-			{/if}
-		</div>
-		<div class="intervalBox">
-			{#if $selectedInterval}
-				<IntervalInfo interval={$selectedInterval} />
-				<button style:background-color="#48f8" on:click={storeInterval}>
-					Store selected interval
-				</button>
-			{:else if $selected.length > 1}
-				<span>{$selected.length} intervals selected</span>
-			{:else}
-				<span>Select an interval</span>
-			{/if}
-		</div>
-		<div class="intervalBox">
-			{#if $storedInterval}
-				<IntervalInfo interval={$storedInterval} />
-				<input type="number" bind:value={factor} min="2" max="19" />
-				<div class="row">
-					<button style:background-color="#48f8" on:click={multiplyStoredInterval}>Multiply</button>
-					<button style:background-color="#48f8" on:click={divideStoredInterval}>Divide</button>
-				</div>
-				<div class="row">
-					<button style:background-color="#48f8" on:click={addToSelected}>Add to selected</button>
-					<button style:background-color="#48f8" on:click={subtractFromSelected}>
-						Subtract from selected
-					</button>
-				</div>
-				<button style:background-color="#f008" on:click={clearStoredInterval}>Clear </button>
-			{:else}
-				<span>Store an interval</span>
-			{/if}
-		</div>
-	</div>
+	<Toolbar />
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="-100 -100 200 200" on:click={unselect}>
 		<g>
@@ -198,7 +88,7 @@
 		</g>
 		<g>
 			{#each $tree as node (node)}
-				<ChromaticCircleNode {node} {playing} {selected} />
+				<ChromaticCircleNode {node} {playing} selected={selectedNotes} />
 			{/each}
 		</g>
 	</svg>
@@ -207,75 +97,9 @@
 <style>
 	.wrapper {
 		display: flex;
-		flex-flow: row wrap;
-	}
-	select {
-		display: block;
-		width: 100%;
-		cursor: pointer;
-		box-sizing: border-box;
-	}
-	input {
-		display: block;
-		width: 100%;
-		box-sizing: border-box;
-	}
-	button {
-		appearance: none;
-		border: none;
-		border-radius: 0.4em;
-		padding: 0.4em 1em;
-		margin: 0.1em;
-		color: black;
-		background-color: #9e9;
-		cursor: pointer;
-	}
-	button:disabled {
-		color: #444;
-		background-color: #bbb;
-		cursor: not-allowed;
-	}
-	.toolbar {
-		flex: 1 0 min-content;
-		display: flex;
-		flex-flow: row wrap;
-		justify-content: stretch;
-		align-items: stretch;
-		padding: 0.4em;
-		gap: 0.4em;
-	}
-	.intervalBox {
-		flex: 1 1 0;
-		display: flex;
-		flex-flow: column nowrap;
-		justify-content: space-between;
-		align-items: stretch;
-		height: 10em;
-		padding: 0.4em;
-		gap: 0.4em;
-		border-radius: 0.4em;
-		background-color: #eee;
-	}
-	.intervalBox button {
-		align-self: stretch;
-		justify-self: end;
-		margin: 0;
-	}
-	.intervalBox span {
-		flex: 1;
-		text-align: center;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	.row {
-		display: flex;
 		flex-flow: row nowrap;
-		justify-content: stretch;
-		gap: 0.4em;
-	}
-	.row > button {
-		flex: 1;
+		align-items: stretch;
+		gap: 0.5em;
 	}
 	svg {
 		max-width: 100vw;
---
+++ src/lib/Toolbar.svelte
@@ -0,0 +1,229 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import type { Readable, Writable } from 'svelte/store';
+	import type { Interval } from './interval';
+	import type { Node } from './intervalTree';
+	import { pureIntervals } from './pureIntervals';
+	import IntervalInfo from './IntervalInfo.svelte';
+
+	const intervalTree = getContext<Readable<Node[]>>('intervalTree');
+	const selectedInterval = getContext<Writable<Interval | null>>('selectedInterval');
+	const storedInterval = getContext<Writable<Interval | null>>('storedInterval');
+	const selectedNotes = getContext<Writable<Node[]>>('selectedNotes');
+
+	$: editing = $selectedNotes.length > 0;
+
+	let intervalIndex = 7;
+	$: interval = pureIntervals[intervalIndex + 1].value;
+	let factor = 2;
+
+	function addNoteUp() {
+		$selectedNotes.forEach((child) => child.addChild(interval));
+	}
+	function addNoteDown() {
+		$selectedNotes.forEach((child) => child.addChild(interval.mul(-1)));
+	}
+	function updateInterval() {
+		$selectedNotes.forEach((child) =>
+			child.setInterval(interval.mul(Math.sign(child.relInterval.log2valueOf()))),
+		);
+	}
+	function selectAll() {
+		$selectedNotes = $intervalTree.slice();
+	}
+	function deleteNotes() {
+		$selectedNotes.forEach((child) => child.removeSelf());
+	}
+
+	function storeInterval() {
+		$storedInterval = $selectedInterval;
+	}
+
+	function addToSelected() {
+		$selectedNotes.forEach((node) => {
+			const base = node.relInterval;
+			const sign = Math.sign(base.log2valueOf());
+			node.setInterval(base.add($storedInterval!.mul(sign)));
+		});
+	}
+	function subtractFromSelected() {
+		$selectedNotes.forEach((node) => {
+			const base = node.relInterval;
+			const sign = Math.sign(base.log2valueOf());
+			node.setInterval(base.sub($storedInterval!.mul(sign)));
+		});
+	}
+	function clearStoredInterval() {
+		$storedInterval = null;
+	}
+	function multiplyStoredInterval() {
+		$storedInterval = $storedInterval!.mul(factor);
+	}
+	function divideStoredInterval() {
+		$storedInterval = $storedInterval!.div(factor);
+	}
+	$: {
+		if ($selectedNotes.length === 2) {
+			$selectedInterval = $selectedNotes[0].absInterval
+				.sub($selectedNotes[1].absInterval)
+				.modSigned()
+				.abs();
+		} else if ($selectedNotes.length > 2) {
+			$selectedInterval = null;
+		}
+	}
+
+	function onKeyDown(event: KeyboardEvent) {
+		if (event.code === 'Backspace') deleteNotes();
+	}
+</script>
+
+<svelte:window on:keydown={onKeyDown} />
+<div class="toolbar">
+	<div class="panel">
+		<select bind:value={intervalIndex}>
+			{#each pureIntervals.slice(1, -1) as pure, i}
+				<option value={i}>
+					{pure.value.frac()} – {pure.name}
+				</option>
+			{/each}
+		</select>
+		<div class="twoButtons">
+			<button on:click={addNoteUp} class="green" disabled={!editing}>Add note up</button>
+			<button on:click={addNoteDown} class="green" disabled={!editing}>Add note down</button>
+		</div>
+		<button on:click={updateInterval} class="blue" disabled={!editing}>Update interval</button>
+		<button on:click={selectAll} class="blue">Select all notes</button>
+		<button on:click={deleteNotes} class="red" disabled={!editing}>Delete</button>
+	</div>
+	<div class="panel">
+		{#if $selectedInterval}
+			<div class="intervalInfo">
+				<IntervalInfo interval={$selectedInterval} />
+			</div>
+			<button class="blue" on:click={storeInterval}> Store selected interval </button>
+		{:else if $selectedNotes.length > 1}
+			<span>{$selectedNotes.length} intervals selected</span>
+		{:else}
+			<span>Select an interval</span>
+		{/if}
+	</div>
+	<div class="panel">
+		{#if $storedInterval}
+			<div class="intervalInfo">
+				<IntervalInfo interval={$storedInterval} />
+			</div>
+			<div class="factorInput">
+				<div class="twoButtons">
+					<button class="blue" on:click={multiplyStoredInterval}>Multiply by</button>
+					<button class="blue" on:click={divideStoredInterval}>Divide by</button>
+				</div>
+				<input type="number" bind:value={factor} min="2" max="19" />
+			</div>
+			<div class="twoButtons">
+				<button class="green" on:click={addToSelected}>Add to selected</button>
+				<button class="green" on:click={subtractFromSelected}> Subtract from selected </button>
+			</div>
+			<button class="red" on:click={clearStoredInterval}>Clear </button>
+		{:else}
+			<span>Store an interval</span>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.toolbar {
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: center;
+		align-items: stretch;
+		gap: 0.5em;
+	}
+	.panel {
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: space-between;
+		align-items: stretch;
+		gap: 0.5em;
+		padding: 0.5em;
+		border-radius: 0.5em;
+		background-color: #ddd;
+	}
+	button {
+		appearance: none;
+		cursor: pointer;
+		border: none;
+		border-radius: 0.5em;
+		padding: 0.8em 1em;
+		color: #000;
+		--alpha: 0.4;
+		--saturation: 80%;
+		--value: 60%;
+		background-color: hsl(var(--hue) var(--saturation) var(--value) / var(--alpha));
+		transition: 0.2s background-color;
+	}
+	button:hover {
+		--alpha: 0.6;
+	}
+	button:disabled {
+		--saturation: 0%;
+		cursor: not-allowed;
+		color: #444;
+	}
+	.twoButtons {
+		display: flex;
+		flex-flow: column nowrap;
+		align-items: stretch;
+	}
+	.twoButtons button:first-child {
+		border-bottom-left-radius: 0;
+		border-bottom-right-radius: 0;
+	}
+	.twoButtons button:last-child {
+		border-top-left-radius: 0;
+		border-top-right-radius: 0;
+	}
+	.red {
+		--hue: 0;
+	}
+	.green {
+		--hue: 120;
+	}
+	.blue {
+		--hue: 220;
+	}
+	.intervalInfo {
+		padding: 0.5em;
+		border-radius: 0.5em;
+		background-color: #fff;
+	}
+	select {
+		appearance: none;
+		cursor: pointer;
+		border: none;
+		padding: 0.5em;
+		border-radius: 0.5em;
+		background-color: #fff;
+	}
+	.factorInput {
+		display: flex;
+		flex-flow: row nowrap;
+	}
+	.factorInput .twoButtons {
+		flex: 1 min-content;
+	}
+	.factorInput button {
+		border-top-right-radius: 0;
+		border-bottom-right-radius: 0;
+	}
+	input {
+		flex: 1;
+		display: block;
+		width: 100%;
+		appearance: none;
+		border: none;
+		padding: 0.5em;
+		border-radius: 0 0.5em 0.5em 0;
+		background-color: #fff;
+	}
+</style>
